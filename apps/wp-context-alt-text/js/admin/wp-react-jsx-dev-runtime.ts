f441b0588fef3c934ae0c5e203dcc67a192fd97f
Daniel Arcé <daniel.arce@gmail.com>
2025-11-08 01:31:56 -0500

    refactor: move files

--- apps/wp-context-alt-text/js/admin/wp-react-jsx-dev-runtime.ts
+++
@@ -1,7 +0,0 @@
-import runtime from "./wp-react-jsx-runtime";
-
-const { Fragment, jsx, jsxs, jsxDEV } = runtime;
-
-export { Fragment, jsx, jsxs, jsxDEV };
-
-export default runtime;
--- apps/wp-context-alt-text/src/Recognition/RecognitionClientException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-namespace ContextAltText\Recognition;
-
-use RuntimeException;
-
-class RecognitionClientException extends RuntimeException {}

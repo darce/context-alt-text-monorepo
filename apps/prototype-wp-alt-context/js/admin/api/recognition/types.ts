7db4922762f835aff706d5062f8229f22b6853ed
Daniel Arcé <daniel.arce@gmail.com>
2025-11-30 20:20:09 -0500

    feat(suggestion): add recognition API client with cluster suggestions endpoint

---
+++ apps/prototype-wp-alt-context/js/admin/api/recognition/types.ts
@@ -0,0 +1,8 @@
+/**
+ * Recognition API Types
+ *
+ * Re-exports all types from modular type files.
+ * @see ./types/ for individual modules
+ */
+
+export * from './types/index';
---
+++ apps/prototype-wp-alt-context/js/admin/api/recognition/types/index.ts
@@ -0,0 +1,10 @@
+/**
+ * Recognition API Types
+ *
+ * Barrel export for all recognition type modules.
+ */
+
+export * from './scan';
+export * from './identity';
+export * from './cluster';
+export * from './suggestion';
